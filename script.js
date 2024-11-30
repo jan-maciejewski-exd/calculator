06617287a93de8cb475e3508e770bbcd2405a5f5
Jan Maciejewski <maciejewski.jan.m@gmail.com>
2024-11-30 17:38:37 +0100

    added comments, added "c" operator functionality

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const display = document.querySelector(".display");
 const numKeys = document.querySelectorAll(".numKeys button");
 const operatorKeys = document.querySelectorAll(".operatorKeys button");
-let operand;
 let operator;
 let result;
 let operand1;
 let operand2;
 
+// Handle number buttons
 numKeys.forEach(numKey => {
     numKey.addEventListener("click", () => {
         if (display.textContent.length < 22) {
-            if (parseFloat(display.textContent) == operand1) {
+            
+            // If there is already an operand or result in memory, pressing button should replace it
+            if (parseFloat(display.textContent) == operand1 || parseFloat(display.textContent) == result) {
                 display.textContent = numKey.textContent; 
             } else {
+            // If current display isn't stored as operand, append to it
                 display.textContent += numKey.textContent;
             };
         } else {
@@ -21,28 +24,47 @@ numKeys.forEach(numKey => {
     });
 });
 
+// Handle operator key presses
 operatorKeys.forEach(operatorKey => {
     operatorKey.addEventListener("click", () => {
-        const currentOperator = operatorKey.textContent;
+        const currentOperator = operatorKey.textContent; // Keep track of the current operator
+
+        // Separate logic for the "Clear" operator
+        if (currentOperator == "C") {
+            display.textContent = "";
+            operand1 = null;
+            operand2 = null;
+            result = null;
+            operator = null;
+            return;
+        }
+
+        // Separate logic for equals - if operand1 and operator exist, perform calculation
         if (currentOperator == "=") {
             if (operand1 !== null && operator !== null) {
-                operand2 = parseFloat(display.textContent);
+                operand2 = parseFloat(display.textContent); // Set second operand from current display
                 calculate(operand1, operand2, operator);
                 operator = null;
+                operand1 = null; // Exeption to the operand1 handling by calculate() - set up clear operand for next operation
             }
         } else {
+
+            // Case when number on display is the first input
             if (operand1 == null) {
                 operand1 = parseFloat(display.textContent);
-            } else if (operator !== null) {
+            } else if (operator !== null) { 
+            //Case when number on display is the second input, perform calculation
                 operand2 = parseFloat(display.textContent);
                 calculate(operand1, operand2, operator);
             };
-            operator = currentOperator;
-            // display.textContent = "";
+            if (currentOperator !== "C") {
+                operator = currentOperator; //Store operator textContent for when calculation needs to be performed
+            }
         };
     });
 });
 
+// Calculating function
 function calculate(op1, op2, operator) {
     if (operator == "+") {
         result = op1 + op2;
@@ -59,5 +81,5 @@ function calculate(op1, op2, operator) {
         };
     };
     display.textContent = result;
-    operand1 = result;
+    operand1 = result; // set the last result as the first operand, for chaining
 };
