0593fdff9820a2c1afb73abb3664c66a930ff081
Jan Maciejewski <maciejewski.jan.m@gmail.com>
2024-11-30 16:32:21 +0100

    improved the calculation logic and operation chain

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ const operatorKeys = document.querySelectorAll(".operatorKeys button");
 let operand;
 let operator;
 let result;
-
+let operand1;
+let operand2;
 
 numKeys.forEach(numKey => {
     numKey.addEventListener("click", () => {
         if (display.textContent.length < 22) {
-            display.append(numKey.textContent);
+            if (parseFloat(display.textContent) == operand1) {
+                display.textContent = numKey.textContent; 
+            } else {
+                display.textContent += numKey.textContent;
+            };
         } else {
             alert("number too long, i stupid")
         };
@@ -18,30 +23,41 @@ numKeys.forEach(numKey => {
 
 operatorKeys.forEach(operatorKey => {
     operatorKey.addEventListener("click", () => {
-        if (operand == null) {
-            operand = parseFloat(display.textContent);
-            display.textContent = "";
-            console.log(operand);
-            operator = operatorKey.textContent;
-            console.log(operator);
+        const currentOperator = operatorKey.textContent;
+        if (currentOperator == "=") {
+            if (operand1 !== null && operator !== null) {
+                operand2 = parseFloat(display.textContent);
+                calculate(operand1, operand2, operator);
+                operator = null;
+            }
         } else {
-            if (operator == "+") {
-                result = operand + parseFloat(display.textContent);
-                display.textContent = result;
-            } else if (operator == "-") {
-                result = operand - parseFloat(display.textContent);
-                display.textContent = result;
-            } else if (operator == "X") {
-                result = operand * parseFloat(display.textContent);
-                display.textContent = result;
-            } else if (operator == "/") {
-                result = operand / parseFloat(display.textContent);
-                display.textContent = result;
+            if (operand1 == null) {
+                operand1 = parseFloat(display.textContent);
+            } else if (operator !== null) {
+                operand2 = parseFloat(display.textContent);
+                calculate(operand1, operand2, operator);
             };
+            operator = currentOperator;
+            // display.textContent = "";
         };
     });
 });
 
-function calculate(operand1, operand2, operator) {
-    return;
-}
+function calculate(op1, op2, operator) {
+    if (operator == "+") {
+        result = op1 + op2;
+    } else if (operator == "-") {
+        result = op1 - op2;
+    } else if (operator == "X") {
+        result = op1 * op2;
+    } else if (operator == "/") {
+        if (op2 !== 0) {
+            result = op1 / op2;
+        } else {
+            alert("cannot divide by 0")
+            result = op1;
+        };
+    };
+    display.textContent = result;
+    operand1 = result;
+};
